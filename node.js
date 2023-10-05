79a1a58deddb5fb845f96b06e639c444ce6a4be7
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-05 11:20:39 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,23 @@
+const { 
+    refreshBearerToken, 
+    checkChargerAvailability,  
+} = require('./ZaptecTeamsNotifier.js');
+
+(async () => {
+    console.log("Starting Zaptec Teams Notifier...");
+   
+    console.log("Setting up intervals for checking charger availability, token refresh...");
+
+    // Check charger availability every 3 minutes
+    setInterval(async () => {
+        await checkChargerAvailability();
+    }, 180000); // 3 minutes
+
+    // Refresh Zaptec token every 24 hours
+    setInterval(async () => {
+        await refreshBearerToken();
+    }, 86400000); // 24 hours
+
+    console.log("Zaptec Teams Notifier is now running!");
+})();
 
