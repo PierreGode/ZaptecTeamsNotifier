f8b6d3feb3d0ba90d3f58fad4af6cfcf6c83e3ca
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-07 21:54:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    startSilentHour: 22,  // Begin silent period at 16:00 (4 PM)
+    startSilentHour: 16,  // Begin silent period at 16:00 (4 PM)
     endSilentHour: 6,    // End silent period at 6:00 (6 AM)
-    silentDays: ['', 'Sunday'] // No notifications on Saturday and Sunday
+    silentDays: ['Saturday', 'Sunday'] // No notifications on Saturday and Sunday
 };
