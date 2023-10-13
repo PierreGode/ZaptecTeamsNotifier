629cbecb105d0be5260bf19fddcd35ac81effc7a
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-13 14:17:08 +0200

    Update node.js

--- node.js
+++ node.js
@@ -11,7 +11,7 @@ const {
     // Check charger availability every 3 minutes
     setInterval(async () => {
         await checkChargerAvailability();
-    }, 180000); // 3 minutes
+    }, config.Zaptechupdateinterval); // 3 minutes
 
     // Refresh Zaptec token every 24 hours
     setInterval(async () => {
