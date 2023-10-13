aeabe9ebc83d760dcb492bdb8f7bb81e8a3e38cb
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-10-13 14:25:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     startSilentHour: 16,  // Begin silent period at 16:00 (4 PM)
     endSilentHour: 6,    // End silent period at 6:00 (6 AM)
     timeZone: 'Europe/Stockholm', // Set timezone for logging
-    silentStart: false, // on service start set if the initial run should be promoting notifications
+    silentStart: true, // on service start set if the initial run should be promoting notifications
     Zaptechupdateinterval: 180000, // set the time between api calls to Zaptec portal: 180000 is 3 minutes between calls 
     silentDays: ['Saturday', 'Sunday'] // No notifications on Saturday and Sunday
 };
